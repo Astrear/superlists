a7336dae9127777314b9552086abe5cf7a116f3c
Astrear <primsark@gmail.com>
2017-06-06 22:03:47 -0500

    Add jquery, qunit test, list.js with keypress listeners

--- superlists/settings.py
+++ superlists/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'lists/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
